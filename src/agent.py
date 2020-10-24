076a3ef7907d60c559a08805597e4395379036c0
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-24 11:53:40 +0000

    solution

--- src/agent.py
+++ src/agent.py
@@ -4,5 +4,7 @@ class Agent:
         self.first_name = first_name
         self.last_name = last_name
 
-    def printout(self):
-        print("My name is " + self.last_name + ", " + self.first_name + " " + self.last_name")
+    def __str__(self):
+      printStr = "My name is " + self.last_name + ", " + self.first_name + " " +self.last_name
+      return printStr
+
